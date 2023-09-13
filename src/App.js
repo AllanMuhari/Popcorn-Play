49b261634d3dc0e268ebb6874c97d82ec3682ae2
AllanMuhari <allanmuhari087@gmail.com>
2023-09-13 03:16:50 +0300

    styling

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import MovieCard from "./Components/Moviecard/MovieCard";
-
+// import { Router } from "react-router";
 
 function App() {
   const API_URL = "https://api.themoviedb.org/3";
   const [movies, setMovies] = useState([]);
-  const fetchMovies = async () => {
+  const [searchKey, setSearchKey] = useState();
+  const fetchMovies = async (searchKey) => {
+    const type = 'discover'
     const {
       data: { results },
-    } = await axios.get(`${API_URL}/discover/movie`, {
+    } = await axios.get(`${API_URL}/${type}/movie`, {
       params: {
         api_key: process.env.REACT_APP_MOVIE_API_KEY,
+        query:searchKey 
       },
     });
     setMovies(results);
@@ -25,17 +28,50 @@ function App() {
   const renderMovies = () =>
     movies.map((movie) => <MovieCard key={movie.id} movie={movie} />);
 
+  const searchMovies = (e) => {
+    e.preventDefault();
+    fetchMovies(searchKey);
+  };
+
   return (
-
     <div>
-      
-     <div className='grid grid-cols-2 lg:grid-cols-4 md:grid-cols-3 max-w-[1620px] m-3 gap-10 text-[18px] font-bold'>
-      {renderMovies()}
+      <div className='m-auto'>
+        <header className=' flex gap-7'>
+          <h1 className='flex m-auto'>Movie Box</h1>
+          <form onSubmit={searchMovies} className="flex ">
+            <input
+              className=' flex  border-2 w-[10rem] lg:w-[30rem] p-1 m-2 lg:mr-[5rem] border-gray-500'
+              type='text'
+              placeholder='What do you want to watch ?'
+              onChange={(e) => setSearchKey(e.target.value)}
+            />
+            <button className="rounded bg-slate-500 w-[4rem] m-3" type={"submit"}>Search</button>
+          </form>
+
+          <div className='hidden lg:flex m-auto'>
+            <h1>Sign in</h1>
+          </div>
+        </header>
+         {searchKey} 
+      </div>
+
+      <div className='moviecards'>
+        <div className='flex'>
+          <h1 className='font-extrabold text-[25px] flex justify-center content-center lg:ml-[10rem]'>
+            Featured Movies
+          </h1>
+          <a
+            href='/'
+            className='cursor-pointer flex m-auto text-[2xl] border-b-2 border-red-600 '
+          >
+            See more
+          </a>
+        </div>
+        <div className='grid grid-cols-2 lg:grid-cols-4 md:grid-cols-2 gap-8 text-[18px] font-bold  lg:mx-72'>
+          {renderMovies()}
+        </div>
+      </div>
     </div>
-
-    </div>
-
-
   );
 }
 
--- src/Components/Moviecard/MovieCard.jsx
+++ src/Components/Moviecard/MovieCard.jsx
@@ -1,17 +1,18 @@
 /** @format */
 
 import React from "react";
-
 const MovieCard = ({ movie }) => {
   const IMAGE_PATH = "https://image.tmdb.org/t/p/w342";
   console.log(movie);
   return (
-    <div className=' grid gri'>
+   <div>
+     <div className='cursor-pointer'>
       {movie.poster_path ? (
         <img src={IMAGE_PATH + movie.poster_path} alt='' />
-      ) : null}
+      ) : <div className="h-[239px] bg-white">No Image found</div>}
       <h3 > {movie.title}</h3>
     </div>
+   </div>
   );
 };
 
