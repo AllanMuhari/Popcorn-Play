c477f648d1c7a01d0c0853303912289fba09f567
AllanMuhari <allanmuhari087@gmail.com>
2023-09-13 13:47:18 +0300

    card

--- src/Components/Moviecard/MovieCard.jsx
+++ src/Components/Moviecard/MovieCard.jsx
@@ -5,14 +5,16 @@ const MovieCard = ({ movie, selectMovie }) => {
   const IMAGE_PATH = "https://image.tmdb.org/t/p/w342";
   console.log(movie);
   return (
-   <div onClick={()=> selectMovie(movie)} className={"movie"}>
-     <div className='cursor-pointer'>
-      {movie.poster_path ? (
-        <img src={IMAGE_PATH + movie.poster_path} alt={movie.title} />
-      ) : <div className="h-[239px] bg-white">No Image found</div>}
-      <h3 > {movie.title}</h3>
+    <div onClick={() => selectMovie(movie)} className={"movie"}>
+      <div className='cursor-pointer'>
+        {movie.poster_path ? (
+          <img src={IMAGE_PATH + movie.poster_path} alt={movie.title} />
+        ) : (
+          <div className='h-[239px] bg-white'>No Image found</div>
+        )}
+        <h3> {movie.title}</h3>
+      </div>
     </div>
-   </div>
   );
 };
 
